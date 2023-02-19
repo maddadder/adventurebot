bcaa99579f669dd08f90d734f280cc33bd18ad42
Charlie Lee <clee@tuta.io>
2023-02-19 10:26:38 -0800

    remove leenetadventurebotui

--- src/Client/Program.cs
+++ src/Client/Program.cs
@@ -1,11 +1,12 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Client;
+using AdventureBotUI.Client;
+using AdventureBotUI.Client.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+builder.Services.AddScoped<AdventureBotReadService>();
 await builder.Build().RunAsync();
